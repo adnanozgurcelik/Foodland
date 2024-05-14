29dc361a89a4b446380214251645eceb2b75020e
Adnan Özgür Çelik <adnanozgurcelik@gmail.com>
2024-05-14 13:53:51 +0300

    Set null check

--- app/src/main/java/com/adnanozgurcelik/foodland/data/remote/dto/search_dto/SearchDto.kt
+++ app/src/main/java/com/adnanozgurcelik/foodland/data/remote/dto/search_dto/SearchDto.kt
@@ -1,8 +1,8 @@
 package com.adnanozgurcelik.foodland.data.remote.dto.search_dto
 
 data class SearchDto(
-    val number: Int,
-    val offset: Int,
-    val results: List<SearchInfoDto>,
-    val totalResults: Int
+    val number: Int?,
+    val offset: Int?,
+    val results: List<SearchInfoDto>?,
+    val totalResults: Int?
 )
