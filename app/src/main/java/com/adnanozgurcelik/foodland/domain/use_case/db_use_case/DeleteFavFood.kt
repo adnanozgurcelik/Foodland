7318b8dff410d77ac25b373945c2017ac231a4c0
Adnan Özgür Çelik <adnanozgurcelik@gmail.com>
2024-05-14 17:02:10 +0300

    Create DeleteFavFood use case class

---
+++ app/src/main/java/com/adnanozgurcelik/foodland/domain/use_case/db_use_case/DeleteFavFood.kt
@@ -0,0 +1,12 @@
+package com.adnanozgurcelik.foodland.domain.use_case.db_use_case
+
+import com.adnanozgurcelik.foodland.data.local.FoodEntity
+import com.adnanozgurcelik.foodland.domain.repository.database.FoodDbRepository
+import javax.inject.Inject
+
+class DeleteFavFood @Inject constructor(
+    private val repository: FoodDbRepository
+) {
+    suspend operator fun invoke(foodEntity: FoodEntity)
+    = repository.deleteFavFood(foodEntity)
+}
