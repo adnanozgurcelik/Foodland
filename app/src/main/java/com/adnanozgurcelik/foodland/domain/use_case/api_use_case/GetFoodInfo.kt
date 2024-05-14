8157d94453c3aa4c4baaf75e665d64a3089ce44a
Adnan Özgür Çelik <adnanozgurcelik@gmail.com>
2024-05-14 15:19:11 +0300

    Create GetFoodInfo use case class

---
+++ app/src/main/java/com/adnanozgurcelik/foodland/domain/use_case/api_use_case/GetFoodInfo.kt
@@ -0,0 +1,25 @@
+package com.adnanozgurcelik.foodland.domain.use_case.api_use_case
+
+import com.adnanozgurcelik.foodland.data.remote.mapper.toFoodInfo
+import com.adnanozgurcelik.foodland.domain.model.info.FoodInfo
+import com.adnanozgurcelik.foodland.domain.repository.api.FoodApiRepository
+import com.adnanozgurcelik.foodland.util.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class GetFoodInfo @Inject constructor(
+    private val repository: FoodApiRepository
+) {
+    suspend operator fun invoke(id: String): Flow<Resource<FoodInfo>>
+    = flow {
+        try {
+            emit(Resource.Loading())
+            val response = repository.getFoodInfo(id)
+            val result = response.toFoodInfo()
+            emit(Resource.Success(result))
+        } catch (e: Exception) {
+            emit(Resource.Error("Error: ${e.localizedMessage}. Cause: ${e.cause}."))
+        }
+    }
+}
