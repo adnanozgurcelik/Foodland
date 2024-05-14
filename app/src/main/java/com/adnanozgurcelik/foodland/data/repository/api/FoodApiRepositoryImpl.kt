9142763efa0715e3b08a029d5cf118a9c8b83ce3
Adnan Özgür Çelik <adnanozgurcelik@gmail.com>
2024-05-14 14:58:01 +0300

    Implement api repository

--- app/src/main/java/com/adnanozgurcelik/foodland/data/repository/api/FoodApiRepositoryImpl.kt
+++ app/src/main/java/com/adnanozgurcelik/foodland/data/repository/api/FoodApiRepositoryImpl.kt
@@ -1,10 +1,20 @@
 package com.adnanozgurcelik.foodland.data.repository.api
 
 import com.adnanozgurcelik.foodland.data.remote.FoodApi
+import com.adnanozgurcelik.foodland.data.remote.dto.info_dto.FoodInfoDto
+import com.adnanozgurcelik.foodland.data.remote.dto.search_dto.SearchDto
 import com.adnanozgurcelik.foodland.domain.repository.api.FoodApiRepository
 import javax.inject.Inject
 
 class FoodApiRepositoryImpl @Inject constructor(
     private val api: FoodApi
 ): FoodApiRepository {
+    override suspend fun getFoodsBySearch(query: String): SearchDto
+    = api.getFoodsBySearch(query = query)
+
+    override suspend fun getFoodsByCuisine(cuisine: String): SearchDto
+    = api.getFoodsByCuisine(cuisine = cuisine)
+
+    override suspend fun getFoodInfo(id: String): FoodInfoDto
+    = api.getFoodInfo(id = id)
 }
