ac9f3d611442adc183b536e3330d86390ba1672f
Adnan Özgür Çelik <adnanozgurcelik@gmail.com>
2024-05-14 14:25:48 +0300

    Add FoodInfo dto to model function

--- app/src/main/java/com/adnanozgurcelik/foodland/data/remote/mapper/FoodMapper.kt
+++ app/src/main/java/com/adnanozgurcelik/foodland/data/remote/mapper/FoodMapper.kt
@@ -1,6 +1,8 @@
 package com.adnanozgurcelik.foodland.data.remote.mapper
 
+import com.adnanozgurcelik.foodland.data.remote.dto.info_dto.FoodInfoDto
 import com.adnanozgurcelik.foodland.data.remote.dto.search_dto.SearchDto
+import com.adnanozgurcelik.foodland.domain.model.info.FoodInfo
 import com.adnanozgurcelik.foodland.domain.model.search.Search
 
 fun SearchDto.toSearch(): List<Search>
@@ -11,3 +13,22 @@ fun SearchDto.toSearch(): List<Search>
         title = results.title ?: ""
     )
 }
+
+fun FoodInfoDto.toFoodInfo(): FoodInfo
+= FoodInfo(
+    dairyFree = dairyFree ?: false,
+    dishTypes = dishTypes ?: emptyList(),
+    extendedIngredients = extendedIngredients ?: emptyList(),
+    glutenFree = glutenFree ?: false,
+    healthScore = healthScore ?: healthScore,
+    id = id ?: 0,
+    image = image ?: "",
+    instructions = instructions ?: "",
+    readyInMinutes = readyInMinutes ?: 0,
+    servings = servings ?: 0,
+    score = score ?: 0.0,
+    summary = summary ?: "",
+    title = title ?: "",
+    vegan = vegan ?: false,
+    vegetarian = vegetarian ?: false,
+)
