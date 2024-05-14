47020b512c9bd0ba34193195b106e93bac3fc1db
Adnan Özgür Çelik <adnanozgurcelik@gmail.com>
2024-05-14 12:42:59 +0300

    Create search dto classes

---
+++ app/src/main/java/com/adnanozgurcelik/foodland/data/remote/dto/search_dto/SearchInfoDto.kt
@@ -0,0 +1,8 @@
+package com.adnanozgurcelik.foodland.data.remote.dto.search_dto
+
+data class SearchInfoDto(
+    val id: Int,
+    val image: String,
+    val imageType: String,
+    val title: String
+)
