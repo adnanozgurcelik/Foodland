6479e6428ccde844d805f1ac9f42e10bdd8fcab4
Adnan Özgür Çelik <adnanozgurcelik@gmail.com>
2024-05-14 13:55:43 +0300

    Create search dto to model mapper function

---
+++ app/src/main/java/com/adnanozgurcelik/foodland/data/remote/mapper/FoodMapper.kt
@@ -0,0 +1,13 @@
+package com.adnanozgurcelik.foodland.data.remote.mapper
+
+import com.adnanozgurcelik.foodland.data.remote.dto.search_dto.SearchDto
+import com.adnanozgurcelik.foodland.domain.model.search.Search
+
+fun SearchDto.toSearch(): List<Search>
+= results!!.map { results ->
+    Search(
+        id = results.id,
+        image = results.image,
+        title = results.title
+    )
+}
