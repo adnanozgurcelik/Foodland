c704d4aef9ed4a32ef72f8dc006a899013cec379
Adnan Özgür Çelik <adnanozgurcelik@gmail.com>
2024-05-15 15:08:45 +0300

    Create Cuisines class

---
+++ app/src/main/java/com/adnanozgurcelik/foodland/presentation/home/Cuisines.kt
@@ -0,0 +1,33 @@
+package com.adnanozgurcelik.foodland.presentation.home
+
+class Cuisines {
+    val cuisines = listOf(
+        "African",
+        "Asian",
+        "American",
+        "British",
+        "Cajun",
+        "Caribbean",
+        "Chinese",
+        "Eastern European",
+        "European",
+        "French",
+        "German",
+        "Greek",
+        "Indian",
+        "Irish",
+        "Italian",
+        "Japanese",
+        "Jewish",
+        "Korean",
+        "Latin American",
+        "Mediterranean",
+        "Mexican",
+        "Middle Eastern",
+        "Nordic",
+        "Southern",
+        "Spanish",
+        "Thai",
+        "Vietnamese"
+    )
+}
