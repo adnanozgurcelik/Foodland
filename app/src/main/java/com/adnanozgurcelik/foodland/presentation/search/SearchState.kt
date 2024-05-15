4cf88a1290fda3abda087bb5f94b4d9f7dd9aa3f
Adnan Özgür Çelik <adnanozgurcelik@gmail.com>
2024-05-15 18:21:01 +0300

    Create SearchState class

---
+++ app/src/main/java/com/adnanozgurcelik/foodland/presentation/search/SearchState.kt
@@ -0,0 +1,12 @@
+package com.adnanozgurcelik.foodland.presentation.search
+
+import com.adnanozgurcelik.foodland.domain.model.search.Search
+
+data class SearchState(
+    val data: List<Search>? = emptyList(),
+    val error: String? = "",
+    val isLoading: Boolean = false,
+    val searchQuery: String? = "",
+    val foodId: String? = "",
+    val toastMessage: String? = ""
+)
