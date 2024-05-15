bcfb9c7d1aabf942090931eca316b576b08a0804
Adnan Özgür Çelik <adnanozgurcelik@gmail.com>
2024-05-15 15:09:04 +0300

    Create HomeState class

---
+++ app/src/main/java/com/adnanozgurcelik/foodland/presentation/home/HomeState.kt
@@ -0,0 +1,12 @@
+package com.adnanozgurcelik.foodland.presentation.home
+
+import com.adnanozgurcelik.foodland.domain.model.info.FoodInfo
+import com.adnanozgurcelik.foodland.domain.model.search.Search
+
+data class HomeState(
+    val data: List<Search>? = emptyList(),
+    val isLoading: Boolean = false,
+    val error: String = "",
+    val toastMessage: String = "",
+    val foodId: String = ""
+)
