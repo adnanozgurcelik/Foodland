b85edb65d9a415752cc51f43f072cc7d17da300c
Adnan Özgür Çelik <adnanozgurcelik@gmail.com>
2024-05-16 13:15:46 +0300

    Create CustomText composable

---
+++ app/src/main/java/com/adnanozgurcelik/foodland/presentation/home/view/composable/CustomText.kt
@@ -0,0 +1,23 @@
+package com.adnanozgurcelik.foodland.presentation.home.view.composable
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+
+@Composable
+fun CustomText(
+    modifier: Modifier = Modifier,
+    content: String,
+    color: Color
+) {
+    Text(
+        text = content,
+        modifier = modifier,
+        style = MaterialTheme.typography.titleLarge,
+        fontWeight = FontWeight.SemiBold,
+        color = color
+    )
+}
