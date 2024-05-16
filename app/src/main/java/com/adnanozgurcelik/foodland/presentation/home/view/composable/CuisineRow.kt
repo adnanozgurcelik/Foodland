a75071ff11b51f5535544be7bb1ecb8e554547a7
Adnan Özgür Çelik <adnanozgurcelik@gmail.com>
2024-05-16 13:42:32 +0300

    Create CuisineRow composable

---
+++ app/src/main/java/com/adnanozgurcelik/foodland/presentation/home/view/composable/CuisineRow.kt
@@ -0,0 +1,33 @@
+package com.adnanozgurcelik.foodland.presentation.home.view.composable
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CuisineRow(
+    content: String,
+    onClick: () -> Unit = {}
+) {
+    Surface(
+        onClick = {
+            onClick.invoke()
+        },
+        modifier = Modifier
+            .padding(start = 20.dp),
+        color = MaterialTheme.colorScheme.background,
+        tonalElevation = 5.dp,
+        shape = RoundedCornerShape(15.dp)
+    ) {
+        Text(
+            text = content,
+            modifier = Modifier
+                .padding(7.dp)
+        )
+    }
+}
