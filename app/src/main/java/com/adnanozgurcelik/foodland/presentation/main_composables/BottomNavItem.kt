5e54e41af8e0bf0380244a9bed29c60632ff23a0
Adnan Özgür Çelik <adnanozgurcelik@gmail.com>
2024-05-16 12:16:09 +0300

    Create BottomNavItem class

---
+++ app/src/main/java/com/adnanozgurcelik/foodland/presentation/main_composables/BottomNavItem.kt
@@ -0,0 +1,9 @@
+package com.adnanozgurcelik.foodland.presentation.main_composables
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomNavItem(
+    val title: String,
+    val unselectedIcon: ImageVector,
+    val selectedIcon: ImageVector
+)
