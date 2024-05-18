ea6e97ca8b8b1ed3799055d5184979a08480dc75
Adnan Özgür Çelik <adnanozgurcelik@gmail.com>
2024-05-18 18:36:37 +0300

    Add clip

--- app/src/main/java/com/adnanozgurcelik/foodland/presentation/search/view/composable/CustomSearchBar.kt
+++ app/src/main/java/com/adnanozgurcelik/foodland/presentation/search/view/composable/CustomSearchBar.kt
@@ -3,12 +3,14 @@ package com.adnanozgurcelik.foodland.presentation.search.view.composable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
@@ -32,7 +34,10 @@ fun CustomSearchBar(
             singleLine = singleLine,
             modifier = Modifier
                 .align(Alignment.CenterStart)
-                .padding(start = 5.dp),
+                .padding(start = 5.dp)
+                .clip(
+                    shape = RoundedCornerShape(15.dp)
+                ),
             decorationBox = { innerTextField ->
                 Box(modifier = Modifier.fillMaxWidth()) {
                     if (value.isEmpty()) {
