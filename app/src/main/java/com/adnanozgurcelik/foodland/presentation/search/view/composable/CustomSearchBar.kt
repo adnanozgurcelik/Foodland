1b79688a65b7b2bf18cf3fa1e2705bf5e4c76afc
Adnan Özgür Çelik <adnanozgurcelik@gmail.com>
2024-05-18 13:06:11 +0300

    Create CustomSearchBar file

---
+++ app/src/main/java/com/adnanozgurcelik/foodland/presentation/search/view/composable/CustomSearchBar.kt
@@ -0,0 +1,49 @@
+package com.adnanozgurcelik.foodland.presentation.search.view.composable
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CustomSearchBar(
+    modifier: Modifier = Modifier,
+    value: String,
+    hint: String,
+    textStyle: TextStyle = MaterialTheme.typography.bodyLarge,
+    maxLines: Int = 1,
+    onValueChanged: (String) -> Unit = {},
+    singleLine: Boolean = true
+) {
+    Box(modifier = modifier) {
+        BasicTextField(
+            value = value,
+            onValueChange = onValueChanged,
+            textStyle = textStyle,
+            maxLines = maxLines,
+            singleLine = singleLine,
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .padding(start = 5.dp),
+            decorationBox = { innerTextField ->
+                Box(modifier = Modifier.fillMaxWidth()) {
+                    if (value.isEmpty()) {
+                        Text(
+                            text = hint,
+                            color = Color.DarkGray
+                        )
+                    }
+                    innerTextField()
+                }
+            }
+        )
+    }
+}
