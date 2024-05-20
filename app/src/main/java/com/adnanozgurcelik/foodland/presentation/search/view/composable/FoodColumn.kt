98df709387987042f7f9defea7c0596b22753bf6
Adnan Özgür Çelik <adnanozgurcelik@gmail.com>
2024-05-20 15:09:35 +0300

    Add favorite button on FoodColumn

--- app/src/main/java/com/adnanozgurcelik/foodland/presentation/search/view/composable/FoodColumn.kt
+++ app/src/main/java/com/adnanozgurcelik/foodland/presentation/search/view/composable/FoodColumn.kt
@@ -3,17 +3,28 @@ package com.adnanozgurcelik.foodland.presentation.search.view.composable
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.adnanozgurcelik.foodland.domain.model.search.Search
@@ -23,6 +34,8 @@ fun FoodColumn(
     search: Search,
     onClick: () -> Unit
 ) {
+    var clicked by remember { mutableStateOf(false) }
+
     Surface(
         onClick = { onClick.invoke() },
         modifier = Modifier
@@ -57,6 +70,30 @@ fun FoodColumn(
             Text(
                 text = search.title
             )
+        }
+        Column(
+            horizontalAlignment = Alignment.End,
+            verticalArrangement = Arrangement.Top
+        ) {
+            Surface(
+                onClick = {
+                    clicked = !clicked
+                },
+                shape = CircleShape
+            ) {
+                if (clicked) {
+                    Icon(
+                        imageVector = Icons.Filled.Favorite,
+                        contentDescription = "Favorite Button",
+                        tint = Color.Red
+                    )
+                } else {
+                    Icon(
+                        imageVector = Icons.Outlined.FavoriteBorder,
+                        contentDescription = "Favorite Button"
+                    )
+                }
+            }
         }
     }
 }
