6855b03961b0089a6ad9c15afe9d81ca901550ff
Adnan Özgür Çelik <adnanozgurcelik@gmail.com>
2024-05-20 15:53:53 +0300

    Create FavoriteColumn composable

---
+++ app/src/main/java/com/adnanozgurcelik/foodland/presentation/favorite/view/composables/FavoriteColumn.kt
@@ -0,0 +1,100 @@
+package com.adnanozgurcelik.foodland.presentation.favorite.view.composables
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import com.adnanozgurcelik.foodland.domain.model.info.FoodInfo
+
+@Composable
+fun FavoriteColumn(
+    foodInfo: FoodInfo,
+    onClick: () -> Unit
+) {
+    var isClicked by remember { mutableStateOf(false) }
+
+    Surface(
+        onClick = { onClick.invoke() },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                start = 20.dp,
+                end = 20.dp,
+                bottom = 10.dp
+            )
+            .height(100.dp),
+        shape = RoundedCornerShape(15.dp),
+        shadowElevation = 8.dp
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Box(
+                modifier = Modifier
+                    .padding(5.dp)
+                    .height(80.dp)
+                    .clip(
+                        shape = RoundedCornerShape(15.dp)
+                    )
+            ) {
+                Image(
+                    painter = rememberImagePainter(data = foodInfo.image),
+                    contentDescription = foodInfo.title
+                )
+            }
+            Text(text = foodInfo.title ?: "")
+        }
+        Column(
+            horizontalAlignment = Alignment.End,
+            verticalArrangement = Arrangement.Top
+        ) {
+            Surface(
+                onClick = {
+                    isClicked = !isClicked
+                          },
+                shape = CircleShape
+            ) {
+                if (isClicked) {
+                    Icon(
+                        imageVector = Icons.Filled.Favorite,
+                        contentDescription = "Favorite Button",
+                        tint = Color.Red
+                    )
+                } else {
+                    Icon(
+                        imageVector = Icons.Outlined.FavoriteBorder,
+                        contentDescription = "Favorite Button"
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+
+
