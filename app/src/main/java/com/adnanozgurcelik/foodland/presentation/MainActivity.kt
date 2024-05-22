059b349387b53975ecbc691f4d609f37d9abda36
Adnan Özgür Çelik <adnanozgurcelik@gmail.com>
2024-05-22 14:43:47 +0300

    Add app navigation

--- app/src/main/java/com/adnanozgurcelik/foodland/presentation/MainActivity.kt
+++ app/src/main/java/com/adnanozgurcelik/foodland/presentation/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.adnanozgurcelik.foodland.presentation.navigation.AppNavigation
 import com.adnanozgurcelik.foodland.presentation.ui.theme.FoodlandTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,7 +22,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FoodlandTheme {
-
+                AppNavigation()
             }
         }
     }
